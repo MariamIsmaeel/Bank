64d46ae93b5064e8b8ed6f67aa2a66857b5fb090
Mariam Ismaeel <mariamismaeel2003@gmail.com>
2025-02-18 17:56:37 +0300

    Add files via upload

---
+++ Customers.java
@@ -0,0 +1,16 @@
+
+public class Customers {
+    private int customerID;
+    private String name;
+    private String address;
+    private String contactInformation;
+
+    public Customers(int customerID, String name, String address, String contactInformation) {
+        this.customerID = customerID;
+        this.name = name;
+        this.address = address;
+        this.contactInformation = contactInformation;
+    }
+
+   
+}
